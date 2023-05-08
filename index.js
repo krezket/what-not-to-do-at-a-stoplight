e048357f35296a1d7d475a279ac4419f020d3370
linotmike <128604219+linotmike@users.noreply.github.com>
2023-05-08 12:58:22 -0700

    added basic file structure for /seeds, public/ and /views for the handlebars

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      // Stored in milliseconds
+      maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+  app.use(session(sess));
+  const hbs = exphbs.create();
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `Server running on port ${PORT}. Visit http://localhost:${PORT}
+      `
+    )
+  );
 });
+
