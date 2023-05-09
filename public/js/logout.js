51ca54428ec7c2d73d0dde80c1db4bf4caea364a
linotmike <128604219+linotmike@users.noreply.github.com>
2023-05-09 02:24:01 -0700

    updates

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,13 +1,12 @@
-const logout = async () => {
-    const response = await fetch('/api/users/login',{
-        method : 'POST',
-        header: {'Content-type':'application.json'} 
-    })
-    if(response.ok){
-        document.location.replace('/')
-    } else{
-        alert('unable to logout')
+document.querySelector("#logout").addEventListener("click", e => {
+  e.preventDefault();
+  fetch("/api/users/logout", {
+    method: "POST",
+  }).then((res) => {
+    if (res.ok) {
+      location.reload();
+    } else {
+      alert("failed to log out");
     }
-}
-
-document.querySelector("#logout").addEventListener('click',logout)
+  });
+});
