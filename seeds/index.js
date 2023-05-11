de42801ad406b563f7496b3f40a83d1573a9315b
Anthony <anthonyguerrero62@outlook.com>
2023-05-11 09:56:27 -0700

    saved

--- seeds/index.js
+++ seeds/index.js
@@ -1,27 +1,27 @@
 const sequelize = require('../config/connection')
 const {Questions ,Choice, Post, Topic, User, Comment} = require('../models')
 
-const userData = require('./userData.json')
+// const userData = require('./userData.json')
+// const topicData = require('./topicData.json')
+// const postData = require('./postData.json')
+// const commentData = require('./commentData.json')
 const questionsData = require('./questionsData.json')
 const choicesData = require('./choiceData.json')
-const topicData = require('./topicData.json')
-const postData = require('./postData.json')
-const commentData = require('./commentData.json')
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
     
-    await User.bulkCreate(userData, {
-      returning: true,
-    });
-    await Topic.bulkCreate(topicData, {
-      returning: true,
-    });
-    await Comment.bulkCreate(commentData, {
-      returning: true,
-    });
-    await Post.bulkCreate(postData, {
-      returning: true,
-    });
+    // await User.bulkCreate(userData, {
+    //   returning: true,
+    // });
+    // await Topic.bulkCreate(topicData, {
+    //   returning: true,
+    // });
+    // await Comment.bulkCreate(commentData, {
+    //   returning: true,
+    // });
+    // await Post.bulkCreate(postData, {
+    //   returning: true,
+    // });
     await Questions.bulkCreate(questionsData, {
       returning: true,
     });
