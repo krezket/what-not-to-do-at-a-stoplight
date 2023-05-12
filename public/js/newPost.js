86ba85df4188a46642a463389ad344175c00e333
Anthony <anthonyguerrero62@outlook.com>
2023-05-12 11:39:18 -0700

    saved

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -26,3 +26,32 @@ document.querySelector("#post-form").addEventListener("submit",e=>{
         }); 
     };
 });  
+
+document.querySelector("#comment-form").addEventListener("submit",e=>{
+    e.preventDefault();
+
+    const searchParam = document.location.pathname.split('/')
+    const [last] = searchParam.slice(-1)
+    post(last);
+    
+    function post(topic) {
+        const postObj = {
+            notes:document.querySelector("#post").value,
+            topic_id:topic
+        }
+        console.log(topic)
+        fetch("/api/posts",{
+            method:"POST",
+            body:JSON.stringify(postObj),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }).then(res=>{
+            if(res.ok){
+               location.reload()
+            } else {
+                alert("bruh")
+            }
+        }); 
+    };
+});  
